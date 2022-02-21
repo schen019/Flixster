3b87bbaa6a47c9484e5d6cc3c36e7b4cc26fad3f
schen019 <schen019@plattsburgh.edu>
2022-02-20 20:21:38 -0500

    Flixster U2 new update

--- app/src/main/java/com/suyi/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/suyi/flixster/adapter/MovieAdapter.java
@@ -1,26 +1,29 @@
 package com.suyi.flixster.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.suyi.flixster.DetailActivity;
 import com.suyi.flixster.R;
 import com.suyi.flixster.models.Movie;
 
-
 import org.chromium.base.Log;
+import org.parceler.Parcels;
 
 import java.util.List;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.VeiwHolder>{
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     Context context;
     List<Movie> movies;
     public MovieAdapter(Context context, List<Movie> movies){
@@ -29,13 +32,13 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.VeiwHolder>{
     }
     @NonNull
     @Override
-    public VeiwHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie,parent,false);
-        return new VeiwHolder(movieView);
+        return new ViewHolder(movieView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VeiwHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Log.d("MovieAdapter","onBindViewHolder" +position);
         Movie movie =movies.get(position);
         holder.bind(movie);
@@ -46,20 +49,23 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.VeiwHolder>{
         return movies.size();
     }
 
-    public class VeiwHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        RelativeLayout container;
         TextView tvTitle;
-        TextView tvOverveiw;
+        TextView tvOverview;
         ImageView ivPoster;
-        public VeiwHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
-            tvOverveiw = itemView.findViewById(R.id.tvOverveiw);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
-            tvOverveiw.setText(movie.getOverview());
+            tvOverview.setText(movie.getOverview());
             String imageUrl;
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
                 imageUrl =movie.getBackdropPath();
@@ -67,6 +73,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.VeiwHolder>{
                 imageUrl =movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
